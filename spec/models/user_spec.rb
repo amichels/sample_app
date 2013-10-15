bd3d8479a32c83beafb96d99b98425199b26e5cb
Andy <andy.j.michels@gmail.com>
2013-10-14 23:09:32 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,10 @@
 require 'spec_helper'
 
-describe "User pages" do
-
-  subject { page }
-
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
-end
-
-describe "profile page" do
-  # Replace with code to make a user variable
-  before { visit user_path(user) }
-
-  it { should have_content(user.name) }
-  it { should have_title(user.name) }
-end
-
 describe User do
 
   before do
     @user = User.new(name: "Example User", email: "user@example.com",
-                   password: "foobar", password_confirmation: "foobar")
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -34,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -116,6 +97,11 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 end
